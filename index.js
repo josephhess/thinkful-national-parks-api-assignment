9de370d4a37652e1574ea9c70a0407856bd1c0dd
josephhess <readysteadyco@gmail.com>
2018-12-12 09:35:13 -0800

    dog images display in browser

--- index.js
+++ index.js
@@ -14,8 +14,23 @@ function getDogsFromApi(count){
   const url = `${BASE_URL}${count}`;
   fetch(url)
     .then(res => res.json())
-    .then(results => console.log(results.message))
+    .then(results => decorateResults(results.message))
     .catch(e => console.log(e));
+}
+
+function decorateResults(results){
+  const decorated = results.map(result => {
+    return decorateSingleResult(result);
+  })
+  $('#results').html(decorated);
+}
+
+function decorateSingleResult(result){
+  return `
+  <li>
+    <img src="${result}" alt="A random dog image">
+  </li>
+`;
 }
 
 $(watchDogFormSubmit);
